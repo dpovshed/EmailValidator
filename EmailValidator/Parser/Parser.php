72d53e46007691d1a48c26834be5e316de1492e1
Dennis Povshedny <basturma@gmail.com>
2023-09-03 23:20:04 +0100

    FIx for PHP7.4

--- EmailValidator/Parser/Parser.php
+++ EmailValidator/Parser/Parser.php
@@ -144,6 +144,10 @@ abstract class Parser
     {
         $previous = $this->lexer->getPrevious();
 
+        if (empty($previous)) {
+            return false;
+        }
+
         if ($previous['type'] === EmailLexer::S_BACKSLASH
             &&
             $this->lexer->token['type'] !== EmailLexer::GENERIC
